6eacdf804f3833382ea455cf62adda99124d0289
saba024 <62515791+saba024@users.noreply.github.com>
2020-03-28 16:13:39 +0300

    Update cached.py

--- Solutions/Task2/853503_Саба_Наскидашвили/cached.py
+++ Solutions/Task2/853503_Саба_Наскидашвили/cached.py
@@ -1,30 +1,19 @@
-import math
-
-def cached(func):
-    previous_args = [None, None]
-    file = open("result.txt",'w')
-    def wrapper(*args, **kwargs):
-        if(previous_args[0] == args):
-            returned = previous_args[1]
-            file.write(str(returned))
-            return returned
-        else:
-            previous_args.clear()
-            previous_args.append(args)
-            previous_args.append(func(*args, **kwargs))
-            returned = previous_args[1]
-            file.write('\n' + str(returned))
-            return returned
+from functools import wraps
+def cached(function):
+    memo = {}
+    @wraps(function)
+    def wrapper(*args):
+        try:
+            return memo[args]
+        except KeyError:
+            rv = function(*args)
+            memo[args] = rv
+            return rv
     return wrapper
 
 @cached
-def add(a, b):
-     return  a + b
+def fibonacci(n):
+    if n < 2: return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
-@cached 
-def plusandpow(a, b, n):
-    return math.pow(a + b, n)     
-
-
-add(5,10)
-plusandpow(3,2,3)
+print(fibonacci(25))
